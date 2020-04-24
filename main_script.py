d9b62d47a314dc1a24583a29f158e8a5685efadd
SyedaAtiyaHusain <53337550+SyedaAtiyaHusain@users.noreply.github.com>
2020-04-25 02:02:25 +0530

    Update and rename script1.py to main_script.py

--- main_script.py
+++ main_script.py
@@ -1,13 +1,25 @@
-from functions import *
-from getpass import getpass
-admin_password="admin"
+""" 
+Developed by:Syeda Atiya Husain
+Requirements:
+            Python version: Python3 or later
+            Necessary Libraries: 1) pickle
+                                 2) getpass
+                                 3) datetime                                 
+"""
+
+from functions import *                                #importing necessary module. 
+from getpass import getpass                            #importing necessary library.
+admin_password="admin"                                 #Librarian can set his/her password here so as to use the system because 
+                                                       #librarian has to login using this password.
 while True:
-    ch=input("Which type of user you are?\n1. Librarian\n2. Faculty\n3. Student\4.Exit")
-    if ch=="1":
+    ch=input("Which type of user you are?\n1. Librarian\n2. Faculty\n3. Student\4.Exit")     #to check which type of user you are.
+    if ch=="1":                                                  #If librarian , then password is required to enter the system.
         password=getpass()
         if password==admin_password:
             while True:
                 v=input("1.Add student\n2.Add Faculty\n3.Add Book\n4.Issue book to student\n5.Issue book to faculty\n6.Return book  by student\n7.return book by faculty\n8.Search book\n9.Print book record\n10.Print Student record\n11.Print faculty record\n12Exit\n-->")
+                                               
+                                                            #librarian has the following menu to perform operations.
                 if v=="1":
                     add_student()
                 elif v=="2":
@@ -36,10 +48,11 @@ while True:
                     break
         else:
             print("Wrong password!")
-    elif ch=="2":
+    elif ch=="2":                                                              #if the user is faculty then this part will be execulted.
         print("Welcome!!")
         while True:
             v=input("1.View user detail\n2.View books detail\n3.Search a book\n4.Exit\n-->")
+                                                                                #faculty has the following menu to perform operations. 
             if v=="1":
                 eid=input("Enter Faculty ID:")
                 fac_disp(eid)
@@ -54,10 +67,11 @@ while True:
                 break
             else:
                 print("Invalid choive")
-    elif ch=="3":
+    elif ch=="3":                                                               #if the student is the user.
         print("Welcome!!")
         while True:
             v=input("1.View user detail\n2.View books detail\n3.Search a book\n4.Exit\n-->")
+                                                                                #student has the following menu to perform operations.
             if v=="1":
                 rollno=input("Enter rollno:")
                 stud_disp(rollno)
@@ -73,7 +87,7 @@ while True:
             else:
                 print("Invalid choive")
             
-    elif ch=="4":
+    elif ch=="4":                                                                                   #if user wants to exit the system.
         print("Closed")
         break
     else:
