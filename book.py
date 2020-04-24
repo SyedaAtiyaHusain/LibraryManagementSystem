a814019a736c06ef6e9e34c8d82ce99c0bcd8f11
SyedaAtiyaHusain <53337550+SyedaAtiyaHusain@users.noreply.github.com>
2020-04-24 23:36:27 +0530

    Update book.py

--- book.py
+++ book.py
@@ -1,4 +1,5 @@
-class Book:
+class Book:                                        #This class is defined so as to create the instance of book which 
+                                                   #will contain the titile, author, number of copies and isbn number, fields.
     def __init__(self,title,author,isbn,copies):
         self.title=title
         self.author=author
