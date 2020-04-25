d76630649dbe8578e122fed328d67a8d58fed9d6
SyedaAtiyaHusain <53337550+SyedaAtiyaHusain@users.noreply.github.com>
2020-04-25 05:49:05 +0530

    Second Commit

--- faculty.py
+++ faculty.py
@@ -1,7 +1,11 @@
-class Faculty:                                     #This class is defined so as to create the instance of faculty which 
-                                                   #will contain the name, faculty id, employee bookissued count 
-                                                   #and dictionary to keep the record of books which faculty has issued, fields.
-    def __init__(self,ename,eid):
+class Faculty:
+  
+  """
+  This class is defined so as to create the instance of faculty which will contain the name,
+  faculty id, employee bookissued count and dictionary to keep the record of books which faculty has issued, fields.
+  """  
+  
+  def __init__(self,ename,eid):
         self.ename=ename
         self.eid=eid
         self.e_bookissued=0
