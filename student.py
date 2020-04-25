91aeafef61f405d4c1fa3bb6ef2ea2a508c1e2f8
SyedaAtiyaHusain <53337550+SyedaAtiyaHusain@users.noreply.github.com>
2020-04-25 05:43:47 +0530

    Second Commit

--- student.py
+++ student.py
@@ -1,6 +1,10 @@
-class Student:                             #This class is defined so as to create the instance of student which will contain the name,
-                                           #branch, admission year, admission ID, roll no, student bookissued count 
-                                           #and dictionary to keep the record of books which student has issued, fields.
+class Student:  
+  
+    """
+    This class is defined so as to create the instance of student which will contain the name,branch, admission year,
+    admission ID, roll no, student bookissued count and dictionary to keep the record of books which student has issued, fields.
+    """
+    
     def __init__(self,s_name,branch,adm_year,adm_id_no,rollno):
         self.s_name=s_name
         self.branch=branch
